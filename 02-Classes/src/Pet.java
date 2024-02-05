e9e4fe8822fba5c7b9ab3b88a9fab05d402cab4e
Omar Arif <oarif@cen-f-oarif.aus.edu>
2024-02-05 09:44:46 +0400

    udpated

--- 02-Classes/src/Pet.java
+++ 02-Classes/src/Pet.java
@@ -1,7 +1,13 @@
 // An interface is a completely "abstract class" 
 // that is used to group related methods with empty bodies: 
 // It can be a way around for multiple inheritance. A class can implement multiple interfaces
+
 public interface Pet {
+	
+	// all variables in an interface are implicitly public, 
+	// static, and final, making them constants.
+	int a = 10;
+	
 	public void walk(); // interfaces have functions with no body
 	public void play(); // interfaces have functions with no body
 }
